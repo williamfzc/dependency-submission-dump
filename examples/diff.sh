fdf1288a71923a57731d2f8c812e85fea8f8d79f
williamfzc <178894043@qq.com>
2023-08-21 21:24:58 +0800

    chore: update example

--- examples/diff.sh
+++ examples/diff.sh
@@ -6,11 +6,11 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Checkout to some branch here
 git checkout ${TARGET}
-DSD_ECOSYSTEM=${ECO} DSD_OUTPUT=target.json dsd
+DSD_ECOSYSTEM=${ECO} DSD_OUTPUT=target.json dsd dump
 
 # Checkout back to the original branch
 git checkout "$current_branch"
-DSD_ECOSYSTEM=${ECO} DSD_OUTPUT=source.json dsd
+DSD_ECOSYSTEM=${ECO} DSD_OUTPUT=source.json dsd dump
 
 # compare
-jsondiffpatch target.json source.json
+dsd diff target.json source.json
